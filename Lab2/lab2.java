61bde68498b7237fba2b3edae0e27090c579c9e5
Storm Randolph <storm.randolph@gmail.com>
2022-04-19 21:28:39 -0700

    edited thirdpass to handled labels and instructions on same line

--- Lab2/lab2.java
+++ Lab2/lab2.java
@@ -3,6 +3,7 @@ package Lab2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class lab2 {
@@ -40,74 +41,86 @@ public class lab2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World"); // prints Hello World
-        secondpass("./Lab2/test2.asm");
+        //secondpass("./Lab2/test2.asm");
     }
 
     // takes filename as input
     // will read file line by line
-    // passes line to line filtering function
-    public static void firstpass(String filename) {
+    // passes line to comment filtering function
+    // creates list of lines without comments
+    // consider: linenums will be one off, won't need to traverse file again
+    public static ArrayList firstpass(String filename) {
         try {
-            int linenum = 1;
+            ArrayList<String> lines = new ArrayList<>();
             File myfile = new File(filename);
             Scanner fileread = new Scanner(myfile);
             while (fileread.hasNextLine()) {
                 // place where parse passes through
                 String line = fileread.nextLine();
-                label(line, linenum);
-                linenum++;
+                // removes comments from code
+                line = filtercomments(line);
+                // adding filtered line to list
+                lines.add(line);
             }
             fileread.close();
+            // return array of lines
+            return lines;
         } catch (FileNotFoundException e) {
             System.out.println("File was not found");
         }
+        return null;
     }
 
+    // takes arraylist of lines in file as input
+    // will read each index and check if it is a label
+    // would we need to account for jumps?
+    public static void secondpass(ArrayList lines) {
+        for (int i = 0; i < lines.size(); i++) {
+            String line = (String) lines.get(i);
+            if (line.contains(":")) {
+                label(line, i + 1);
+            }
+        }
+    }
 
     // takes filename as input
     // will read file line by line
     // passes line to line filtering function
-    public static void secondpass(String filename) {
-        try {
-            int linenum = 1;
-            File myfile = new File(filename);
-            Scanner fileread = new Scanner(myfile);
-            while (fileread.hasNextLine()) {
-                // place where parse passes through
-                String line = fileread.nextLine();
-                // remove comments and whitespace from line
+    public static void thirdpass(ArrayList lines) {
+        String test[];
+        for (int i = 0; i < lines.size(); i++) {
+            String line = (String) lines.get(i);
+            // if label and j/jal instruction are on the same line
+            // replaces colon with whitespaces and splits into list
+            if (line.contains(":") && !line.contains("$")) {
+                line = line.trim();
+                line = line.replace(":", " ");
+                test = line.split(" ");
+            }
+            // if label and any other instruction are on the same line
+            else if (line.contains(":") && line.contains("$")) {
+                line = filter(line);
+                line = line.replace(":", " ");
+                line = line.replace("$", " $");
+                test = line.split(" ");
+            }
+            // if line doesn't contain label but is j/jal instruction
+            else if (!line.contains("$") && line != "") {
+                line = line.trim();
+                test = line.split(" ");
+            }
+            // if line contains any other instruction other than j/jal
+            else {
+                // remove whitespace from line
                 line = filter(line);
                 // strip whitespace
                 line = line.replaceAll("\\s", "");
-                // test to make sure line is properly filtered at this point
-                if (line != "") {
-                    System.out.println(line);
-                }
-                // check if present in hashtable, if it is go to that line + 1(label handling)
-                // check other hashmap for instruction, registers, and binary conversion
-                linenum++;
-            }
-            fileread.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File was not found");
-        }
-    }
-
-    // finds line or lines that contain labels
-    // will have these two numbers into hashmap upon ...
-    public static String filter(String line) {
-        String filtered = "";
-        for (int i = 0; i < line.length(); i++) {
-            String x = Character.toString(line.charAt(i));
-            if (!x.contains("#")) {
-                if (!x.contains("\\s+") && !x.contains(" ") && !x.contains("\n")) {
-                    filtered += Character.toString(line.charAt(i));
-                }
-            } else {
-                return filtered;
+                line = line.replace("$", " $");
+                test = line.split(" ");
             }
         }
-        return filtered;
+        // check if present in hashtable, if it is go to that line + 1(label handling)
+        // check other hashmap for instruction, registers, and binary conversion
     }
 
     // finds line or lines that contain labels
@@ -117,6 +130,32 @@ public class lab2 {
             System.out.println(line);
             System.out.println(linenum);
         }
+    }
+
+    // filters out comments from line
+    public static String filtercomments(String line) {
+        String filtered = "";
+        for (int i = 0; i < line.length(); i++) {
+            String x = Character.toString(line.charAt(i));
+            if (!x.contains("#")) {
+                filtered += Character.toString(line.charAt(i));
+            } else {
+                return filtered;
+            }
+        }
+        return filtered;
+    }
+
+    // filters out whitespace from line
+    public static String filter(String line) {
+        String filtered = "";
+        for (int i = 0; i < line.length(); i++) {
+            String x = Character.toString(line.charAt(i));
+            if (!x.contains("\\s+") && !x.contains(" ") && !x.contains(",")) {
+                filtered += Character.toString(line.charAt(i));
+            }
+        }
+        return filtered;
     }
 
     // initialize opcode hashmap
