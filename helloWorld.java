2ffe9129bf8f42a2dd3643cac7e4600bc7a4f446
Nathan Jaggers <nathanj8910@gmail.com>
2022-04-25 01:28:52 -0700

    hello world

--- helloWorld.java
+++ helloWorld.java
@@ -10,22 +10,6 @@ public class helloWorld {
     public static void main(String []args) {
        System.out.println("Hello World"); // prints Hello World
 
-       ArrayList<String> test = new ArrayList<>();
-
-       ArrayList<String> test1 = new ArrayList<>();
-
-       test = newlist(test1);
-
     }
 
-    public static ArrayList<String> newlist(ArrayList<String> data)
-    {
-       data.add("one");
-       data.add("2");
-       data.add("3");
-       data.add("4");
-       data.add("5");
-
-       return data;
-    }
  }
