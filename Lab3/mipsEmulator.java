6f0fc19c5b477e41950cf34ee2e68a02650a0fce
Storm Randolph <storm.randolph@gmail.com>
2022-04-29 09:23:04 -0700

    test

--- Lab3/mipsEmulator.java
+++ Lab3/mipsEmulator.java
@@ -124,7 +124,14 @@ public class mipsEmulator {
 
    public void help()
    {
-
+      System.out.println("h = show help");
+      System.out.println("d = dump register state");
+      System.out.println("s = single step through the program (i.e. execute 1 instruction and stop)");
+      System.out.println("s num = step through num instructions of the program");
+      System.out.println("r = run until the program ends");
+      System.out.println("m num1 num2 = display data memory from location num1 to num2");
+      System.out.println("c = clear all registers, memory, and the program counter to 0");
+      System.out.println("q = exit the program");
    }
 
    public void dump()
