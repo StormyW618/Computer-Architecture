8c6472b2101e14e30a1f740f537085d522ffc016
Nathan Jaggers <nathanj8910@gmail.com>
2022-05-23 10:01:39 -0700

    getting a small start on Lab 5

---
+++ Lab5/Lab5.java
@@ -0,0 +1,21 @@
+/**
+ * Programmers: Nathan Jaggers, Storm Randolph
+ * 
+ * CPE 315 - Computer Architecture
+ * Dr.Seng
+ * Spring 2022
+ * 
+ * Lab # - Title
+ * 
+ * Description:
+ */
+
+package Lab5;
+
+public class Lab5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+
+}
---
+++ Lab5/mipsPredictor.java
@@ -0,0 +1,32 @@
+/**
+ * Programmers: Nathan Jaggers, Storm Randolph
+ * 
+ * CPE 315 - Computer Architecture
+ * Dr.Seng
+ * Spring 2022
+ * 
+ */
+
+package Lab5;
+
+import Lab2.Instruction;
+import Lab2.mipsAssembler;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class mipsPredictor {
+
+    /****************************************************
+    * Class is to organize data and simplify process of
+    * emulating assembly program.
+    ****************************************************/
+
+   // ---MEMBERS---
+
+   // ---METHODS---
+   // constructors
+
+   //
+
+}
