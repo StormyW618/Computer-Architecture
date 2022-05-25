f592428652d7ad954a3f6ae8708026aa5d54eb6b
Storm Randolph <storm.randolph@gmail.com>
2022-05-25 14:57:58 -0700

    updated

--- Lab5/mipsPredictor.java
+++ Lab5/mipsPredictor.java
@@ -16,81 +16,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 //---to do---
-    //maybe fill ghr and prediction table with zeros in constructors?
-    //make function to help with updating prediction table?
-    //function to get prediction or just do it in step? maybe get/update prediction is same function?
-
+//maybe fill ghr and prediction table with zeros in constructors?
+//make function to help with updating prediction table?
+//function to get prediction or just do it in step? maybe get/update prediction is same function?
 
 public class mipsPredictor extends mipsEmulator {
 
     /****************************************************
-    * Class is to organize data and simplify process of
-    * predicting branches as correlating branch predictor
-    * for assembly programs.
-    ****************************************************/
+     * Class is to organize data and simplify process of
+     * predicting branches as correlating branch predictor
+     * for assembly programs.
+     ****************************************************/
 
     // ---MEMBERS---
-    public int predictionsCount; //amount of predictions made in program
-    public int predictionsCorrect; //amount of predictions made correctly in program
-    public int ghrSize; //how long we want the GHR shift register to be
-    public ArrayList<Integer> ghr; // Global History Register  
+    public int predictionsCount; // amount of predictions made in program
+    public int predictionsCorrect; // amount of predictions made correctly in program
+    public int ghrSize; // how long we want the GHR shift register to be
+    public ArrayList<Integer> ghr; // Global History Register
     public ArrayList<Integer> predictionTable; // table to hold 2-bit prediction value
 
     // ---METHODS---
     // constructors
     public mipsPredictor() {
-    // run previous constructor
-    super();
+        // run previous constructor
+        super();
 
-    // initialize members
-    predictionsCount = 0; 
-    predictionsCorrect = 0;
-    ghrSize = 2;
-    ghr = new ArrayList<>();  
-    predictionTable = new ArrayList<>();
+        // initialize members
+        predictionsCount = 0;
+        predictionsCorrect = 0;
+        ghrSize = 2;
+        ghr = new ArrayList<>();
+        predictionTable = new ArrayList<>();
 
- }
+    }
 
     public mipsPredictor(ArrayList<Instruction> assembledProgram) {
-    // run previous constructor
-    super(assembledProgram);
+        // run previous constructor
+        super(assembledProgram);
 
-    // initialize members
-    predictionsCount = 0; 
-    predictionsCorrect = 0;
-    ghrSize = 2;
-    ghr = new ArrayList<>();  
-    predictionTable = new ArrayList<>();
+        // initialize members
+        predictionsCount = 0;
+        predictionsCorrect = 0;
+        ghrSize = 2;
+        ghr = new ArrayList<>();
+        predictionTable = new ArrayList<>();
 
- }
+    }
 
     public mipsPredictor(mipsAssembler assembled) {
-    // run previous constructor
-    super(assembled);
+        // run previous constructor
+        super(assembled);
 
-    // initialize members
-    predictionsCount = 0; 
-    predictionsCorrect = 0;
-    ghrSize = 2;
-    ghr = new ArrayList<>();  
-    predictionTable = new ArrayList<>();
+        // initialize members
+        predictionsCount = 0;
+        predictionsCorrect = 0;
+        ghrSize = 2;
+        ghr = new ArrayList<>();
+        predictionTable = new ArrayList<>();
 
-
- }
+    }
 
     public mipsPredictor(mipsAssembler assembled, int inputSize) {
-    // run previous constructor
-    super(assembled);
+        // run previous constructor
+        super(assembled);
 
-    // initialize members
-    predictionsCount = 0; 
-    predictionsCorrect = 0;
-    ghrSize = inputSize;
-    ghr = new ArrayList<>();  
-    predictionTable = new ArrayList<>();
+        // initialize members
+        predictionsCount = 0;
+        predictionsCorrect = 0;
+        ghrSize = inputSize;
+        ghr = new ArrayList<>();
+        predictionTable = new ArrayList<>();
 
-
- }
+    }
 
     // user commands
     @Override
@@ -115,7 +112,7 @@ public class mipsPredictor extends mipsEmulator {
                 // dump register state
                 dump();
                 break;
-            
+
             case 'b':
                 // print branch predictions
                 predictions();
@@ -126,19 +123,19 @@ public class mipsPredictor extends mipsEmulator {
                 // single or multiple based on input
                 String[] splitInput = userInput.split(" ");
                 if (splitInput.length == 1) {
-                // run step command for 1 iteration
-                step(1);
+                    // run step command for 1 iteration
+                    step(1);
                 } else if (splitInput.length == 2) {
-                splitInput[1] = splitInput[1].trim();
-                try {
-                    // get number of steps after s command
-                    step(Integer.parseInt(splitInput[1]));
-                } catch (NumberFormatException e) {
-                    System.out.println("Second argument not a number");
-                }
+                    splitInput[1] = splitInput[1].trim();
+                    try {
+                        // get number of steps after s command
+                        step(Integer.parseInt(splitInput[1]));
+                    } catch (NumberFormatException e) {
+                        System.out.println("Second argument not a number");
+                    }
 
                 } else {
-                System.out.println("Too many arguments, try again.");
+                    System.out.println("Too many arguments, try again.");
                 }
 
                 break;
@@ -153,16 +150,16 @@ public class mipsPredictor extends mipsEmulator {
                 // split string into list to properly parse out args num1 and num2
                 String[] arr = userInput.split(" ");
                 if (arr.length == 1) {
-                memory(0, 0);
+                    memory(0, 0);
                 } else if (arr.length == 2) {
-                int num1 = Integer.parseInt(arr[1]);
-                memory(num1, num1);
+                    int num1 = Integer.parseInt(arr[1]);
+                    memory(num1, num1);
                 } else if (arr.length == 3) {
-                int num1 = Integer.parseInt(arr[1]);
-                int num2 = Integer.parseInt(arr[2]);
-                memory(num1, num2);
+                    int num1 = Integer.parseInt(arr[1]);
+                    int num2 = Integer.parseInt(arr[2]);
+                    memory(num1, num2);
                 } else {
-                System.out.println("Too many arguments, try again.");
+                    System.out.println("Too many arguments, try again.");
                 }
 
                 break;
@@ -204,32 +201,40 @@ public class mipsPredictor extends mipsEmulator {
     public void step(int numOfSteps) {
         // uses program array list and executes
         // a specified number of instructions. Number
-        // of instructions process id determined by 
-        // value passed into function 
+        // of instructions process id determined by
+        // value passed into function
 
-        //keep track of steps executed
-        int stepsExecuted;  
-        for (stepsExecuted = 0; stepsExecuted < numOfSteps; stepsExecuted++)
-        {
-            if(pc < program.size())
-            {
-                //make prediction and increment counter
+        // keep track of steps executed
+        int stepsExecuted;
+        boolean result;
+        for (stepsExecuted = 0; stepsExecuted < numOfSteps; stepsExecuted++) {
+            if (pc < program.size()) {
 
-                //check if correct, if so increment counter
+                // if instruction is a branch
+                if (((program.get(pc).instruct == "beq") || (program.get(pc).instruct == "beq"))) {
+                    // get index for update prediction
+                    int index = ghr2index();
+                    // make prediction and increment counter
+                    result = updateprediction(index);
 
-                //update ghr
+                    // update ghr
+                    if (result == true) {
+                        ghrShift(1);
+                    } else {
+                        ghrShift(0);
+                    }
 
-                //run instruction
+                }
+
+                // run instruction
                 executeInst();
-            }
-            else
-            {
+            } else {
                 break;
             }
 
         }
 
-        System.out.printf("\t%d instruction(s) executed\n",stepsExecuted);
+        System.out.printf("\t%d instruction(s) executed\n", stepsExecuted);
     }
 
     @Override
@@ -242,61 +247,130 @@ public class mipsPredictor extends mipsEmulator {
         Arrays.fill(registers, 0);
 
         // reset prediction values
-        predictionsCount = 0; 
+        predictionsCount = 0;
         predictionsCorrect = 0;
-        ghr.clear();  
+        ghr.clear();
         predictionTable.clear();
 
         // print out notification
         System.out.println("\tSimulator reset");
     }
 
-    public void predictions()
-    {
-            //prints out prediction results of loaded program
+    public void predictions() {
+        // prints out prediction results of loaded program
 
-            //example output
-            //accuracy 61.79% (8360 correct predictions, 13529 predictions)
+        // example output
+        // accuracy 61.79% (8360 correct predictions, 13529 predictions)
 
-            System.out.printf("accuracy %f%% (%d correct predictions, %d predictions)",(predictionsCorrect/predictionsCount)*100, predictionsCorrect, predictionsCount);
+        System.out.printf("accuracy %f%% (%d correct predictions, %d predictions)",
+                (predictionsCorrect / predictionsCount) * 100, predictionsCorrect, predictionsCount);
     }
 
     // prediction functions
-    public int ghr2index()
-    {
-        if(ghr.isEmpty())
-        {
+    public int ghr2index() {
+        if (ghr.isEmpty()) {
             return 0;
-        }
-        else{
-            //store array as string
+        } else {
+            // store array as string
             String temp = ghr.toString();
 
-            //replace parts of string and convert to binary value
-            temp = temp.replace('[',' ');
-            temp = temp.replace(']',' ');
-            temp = temp.replace(',',' ');
-            temp = temp.replaceAll(" ","");
+            // replace parts of string and convert to binary value
+            temp = temp.replace('[', ' ');
+            temp = temp.replace(']', ' ');
+            temp = temp.replace(',', ' ');
+            temp = temp.replaceAll(" ", "");
 
-            //parse out integer from binary string 
-            int num = Integer.parseInt(temp,2);
+            // parse out integer from binary string
+            int num = Integer.parseInt(temp, 2);
 
-            //return index
+            // return index
             return num;
         }
     }
 
-    public void ghrShift(int input)
-    {
-        //shift everyting down, make space for new input
-        for (int i = ghrSize-1; i > 0; i--)
-        {
-            ghr.set(i, ghr.get(i-1));
+    public void ghrShift(int input) {
+        // shift everyting down, make space for new input
+        for (int i = ghrSize - 1; i > 0; i--) {
+            ghr.set(i, ghr.get(i - 1));
         }
 
-        //insert new value
+        // insert new value
         ghr.set(0, input);
     }
 
+    // updates prediction function
+    // takes in prediction from table using address given by ghr
+    // have int or string taken to determine how prediction should be updated(ex.
+    // flowchart from lecture)
+    public boolean updateprediction(int index) {
+
+        // cases dependent on if taken or not
+        // prediction from prediction table
+
+        int prediction = predictionTable.get(index);
+
+        // check if correct
+        boolean correct = false;
+        if (program.get(pc).instruct == "beq") {
+            if (registers[program.get(pc).rs] == registers[program.get(pc).rt]) {
+                if (prediction > 1) {
+                    correct = true;
+                }
+            }
+
+        } else if (program.get(pc).instruct == "bne") {
+            if (registers[program.get(pc).rs] != registers[program.get(pc).rt]) {
+                if (prediction > 1) {
+                    correct = true;
+                }
+            }
+        }
+
+        // strongly nt
+        if (prediction == 0) {
+            // increment prediction table if prediction false
+            // if correct, do nothing
+            if (correct == false) {
+                predictionTable.set(index, 1);
+            }
+
+        }
+
+        // weakly not taken
+        if (prediction == 1) {
+            // if prediction is not taken
+            // if prediction true, decrement
+            // if prediction false, increment
+            if (correct == true) {
+                predictionTable.set(index, 0);
+            } else {
+                predictionTable.set(index, 2);
+            }
+
+        }
+
+        // weakly t
+        if (prediction == 2) {
+            // if prediciton taken
+            // if prediction true, increment
+            // else, decrement prediction
+            if (correct == true) {
+                predictionTable.set(index, 3);
+            } else {
+                predictionTable.set(index, 1);
+            }
+        }
+
+        // strongly taken
+        if (prediction == 3) {
+            // if prediction taken
+            // if prediction true, do nothing
+            // else, decrement prediction
+            if (correct == false) {
+                predictionTable.set(index, 2);
+            }
+        }
+        return correct;
+    }
 
 }
