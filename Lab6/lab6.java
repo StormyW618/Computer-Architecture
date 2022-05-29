22cebd6fd5a430572f834853a874b47400198267
Nathan Jaggers <nathanj8910@gmail.com>
2022-05-29 15:02:33 -0700

    init for Lab6

---
+++ Lab6/lab6.java
@@ -0,0 +1,21 @@
+/**
+ * Programmers: Nathan Jaggers, Storm Randolph
+ * 
+ * CPE 315 - Computer Architecture
+ * Dr.Seng
+ * Spring 2022
+ * 
+ * Lab 6 - Cache Simulator
+ * 
+ * Description:
+ */
+
+package Lab6;
+
+public class lab6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+
+}
